fdad565816d9b2e4a80eed02b7005fb615875c08
devkus-dz <btao.apps@gmail.com>
2026-02-27 22:58:16 +0100

    updating env variable in next.config file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   async rewrites() {
 
-    const backendUrl = process.env.BACKEND_URL || 'http://localhost:5000';
+    const backendUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
 
     return [
       {
